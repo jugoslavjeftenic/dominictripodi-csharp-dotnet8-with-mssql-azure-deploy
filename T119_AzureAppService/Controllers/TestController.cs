4080e14ea9bea9e4e7601a5bc6390c6771979b10
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-13 23:01:27 +0100

    Release build and Azure deploy

---
+++ T119_AzureAppService/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using T119_AzureAppService.Data;
+
+namespace T119_AzureAppService.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TestController(IConfiguration config) : ControllerBase
+	{
+		private readonly DataContextDapper _dapper = new(config);
+
+		[HttpGet]
+		public string TestApplication()
+		{
+			return "Congratulation, your Application is up & running.";
+		}
+
+		[HttpGet("Connection")]
+		public DateTime TestConnection()
+		{
+			return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+		}
+	}
+}
